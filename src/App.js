ec7bb5e4850c90531f99ff798212a2cd8ee1e65b
Arnold Mavhezha <arnoemavhezha@gmail.com>
2023-05-16 15:13:26 +0200

    code done!

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [name, setName] = useState("Anorld");
+
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="Box">
+        <h1>How to use useState()</h1>
+        <p>My name is {name}</p>
+        <button onClick={() => setName("Blonatron") }>Click me to change name!</button> 
+      </div>
     </div>
   );
 }
